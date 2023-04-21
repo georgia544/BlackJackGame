374ead598d86ca15c9ca3c72634a538d8fddb4c6
georgia544 <diana.shakh98@gmail.com>
2023-04-21 19:56:09 +0400

    Initial commit

---
+++ app/src/main/java/com/blackjackgame/app/BlackJackGame.kt
@@ -0,0 +1,21 @@
+package com.blackjackgame.app
+
+interface BlackJackGame {
+    fun startGame() // начало игры
+
+    fun getStartingCards(): List<Card> // берем 2 карты
+
+    fun getAdditionalCard(): Card // взять еще одну карту если надо
+    fun stopGettingCards() // перестаем брать карты
+
+    fun getCardsValue(): Int // показываем сумму карт
+
+    fun getOpponentCards(): List<Card> // показывает карты соперника
+    fun getOpponentCardsValue(): Int // показывает сумму карт соперника
+
+    fun checkIfWin(): Boolean // проверяем кто выиграл
+
+    fun getStats(): List<Int> // показывает статистику игр
+}
+
+data class Card(val name: String, val value: Int)
