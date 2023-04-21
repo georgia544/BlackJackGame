b99edc5dad64c1cbef5f7d41f9443092a14fa710
georgia544 <diana.shakh98@gmail.com>
2023-04-21 20:39:13 +0400

    refactor Card class

--- app/src/main/java/com/blackjackgame/app/Card.kt
+++ app/src/main/java/com/blackjackgame/app/Card.kt
@@ -1,3 +1,19 @@
 package com.blackjackgame.app
 
-data class Card(val name: String, val value: Int)
+data class Card(val name: CardRank, val value: Int)
+
+enum class CardRank{
+    ACE,
+    KING,
+    QUEEN,
+    JACK,
+    TEN,
+    NINE,
+    EIGHT,
+    SEVEN,
+    SIX,
+    FIVE,
+    FOUR,
+    THREE,
+    TWO
+}
